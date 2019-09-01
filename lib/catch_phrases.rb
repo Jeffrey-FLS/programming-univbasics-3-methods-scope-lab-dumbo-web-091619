06854a2f9d3d82b76fd493b5a658cf7d3bea7a26
Jeffrey Paulino <jeffreypaulino@gmail.com>
2019-09-01 14:58:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,3 +6,11 @@ end
 def toadstool
   puts "Thank You Mario! But Our Princess Is In Another Castle!"
 end
+
+def toadstool
+  puts "Thank You Mario! But Our Princess Is In Another Castle!"
+end
+
+def toadstool
+  puts "Thank You Mario! But Our Princess Is In Another Castle!"
+end
